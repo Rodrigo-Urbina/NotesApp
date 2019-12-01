7a3d2ff2e67f17ea3ff0d2a937ff7abbe7ca1c63
Rodrigo-Urbina <rodrigourbinadelac@yahoo.com.mx>
2019-12-01 04:03:28 -0600

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,8 +1,44 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+const express = require('express');
+const path = require('path'); // permitirnos manejar mas facilmente las rutas de las vistas
+const exphbs = require('express-handlebars'); 
+const methover = require('method-override'); // incluye metodos put y delete en formularios
+const expsess = require('express-session'); //autenticar usuario con credenciales de manera temporal
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+// Initializing
+const app = express();
+require('./db');
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}))
+app.set('view engine', '.hbs');
+
+// Middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(methover('_method'));
+app.use(expsess({
+    secret: 'admin',
+    resave: true,
+    saveUninitialized: true
+}))
+// Global Variables
+
+
+// Routes
+app.use(require('./routes/index'));
+app.use(require('./routes/notes'));
+app.use(require('./routes/users'));
+
+// Static Files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Server hosting
+app.listen(app.get('port'), () => {
+    console.log('Server running on port', app.get('port'));
+});
