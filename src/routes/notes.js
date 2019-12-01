eafdff7baac2bb22d4179ab104dd4f4dadc9d1db
Rodrigo-Urbina <rodrigourbinadelac@yahoo.com.mx>
2019-12-01 14:58:09 -0600

    Add files via upload

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/notes/add', (req, res) => {
+    res.render('notes/addnote');
+});
+
+router.post('/notes/new', (req, res) => {
+    const { title, description } = req.body;
+    const errors = [];
+
+    if (!title) {
+        errors.push({ text: 'Title field is missing' });
+    }
+
+    if (!description) {
+        errors.push({ text: "Description field is missing" });
+    }
+
+    if (errors.length > 0) {
+        res.render('notes/addnote', {
+            errors,
+            title,
+            description
+        });
+    } else {
+        res.send('ok');
+    }
+});
+
 router.get('/notes', (req, res) => {
     res.send('NOTES');
 });
