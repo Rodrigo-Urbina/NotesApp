4ae7f273d90234608c68f2d25dae37704c337dbe
Rodrigo-Urbina <rodrigourbinadelac@yahoo.com.mx>
2019-12-01 22:45:46 -0600

    Add files via upload

--- src/db.js
+++ src/db.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/todolist', {
+mongoose.connect('mongodb+srv://admin:admin@todolist-yavw8.mongodb.net/test?retryWrites=true&w=majority', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false
 })
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
+
+/*mongoose.connect('mongodb://localhost/todolist', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
+    .then(db => console.log('DB is connected'))
+    .catch(err => console.error(err));*/
